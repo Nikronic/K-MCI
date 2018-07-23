ecd9556f4b2e6ec341ddb06fdfc94b452f583746
M. Doosti Lakhani <nikan.doosti@outlook.com>
2018-07-23 05:30:53 +0430

    preprocessing iris dataset

--- datasets.py
+++ datasets.py
@@ -45,9 +45,26 @@ def cmc():
     y = dataset.iloc[:,-1].values # target values
     return (x,y)
 
+
 def glass():
-     # importing dataset
+    # importing dataset
     dataset = pd.read_csv(path+'glass.csv', names= ['Id','refractive index','Magnesium','Aluminum','Silicon','Potassium','Calcium','Barium','Iron','glass'])
     x = dataset.iloc[:,:-1].values # features
     y = dataset.iloc[:,-1].values # target values
     return (x,y)
+
+
+def iris():
+    # importing dataset
+    dataset = pd.read_csv(path+'iris.csv', names= ['sepal length','sepal width','petal length','petal width','class'])
+    x = dataset.iloc[:,:-1].values # features
+    y = dataset.iloc[:,-1].values # target values
+    
+    # encoding categorial data types to labelEncoder
+    from sklearn.preprocessing import LabelEncoder
+    labelencoder_y = LabelEncoder()
+    labelencoder_y = labelencoder_y.fit(y)
+    y = labelencoder_y.transform(y)  # 0 for 'Iris-setosa', 1 for 'Iris-versicolor', 2 for 'Iris-virginica'
+    return (x,y)
+
+
