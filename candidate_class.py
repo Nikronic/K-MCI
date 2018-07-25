891af1fac80f298dceac8ed8d56ce2c73693ac5e
M. Doosti Lakhani <nikan.doosti@outlook.com>
2018-07-26 03:24:11 +0430

    restructuring class and it's parameters

--- candidate_class.py
+++ candidate_class.py
@@ -4,28 +4,29 @@ Created on Thu Jul 26 01:11:10 2018
 
 @author: Mohammad Doosti Lakhani
 """
-class Candidate:
-
-    def __init__(self,features,psi, t, r,fitness=-1):
-        self.features = features
-        self.psi = psi
-        self.t = t
-        self.r = r
-        self.fitness = fitness
-
-        
-    def show_info(self):
-        print("Candidate with features={}, fitness={}".format(self.features,self.fitness))
-        
-    def random_generator(self):
-        
-
-
-""" numberOfVariations = t
-    iterationCount = n
-    convergencePar = ep
-    samplingIntervalReduction = r
-    samplingInterval = psi
-    candidatesCount   
+"""
+This is the implementatino of Candidate class. Candidate class is the most important class because it has
+the structure of answer based on cluster centers and other important parameters to control explotation and exploration. 
 """
 
+
+# importing libraries
+import random
+
+
+class Candidate:
+    def __init__(self,clusters,centers,sampling_interval, variation_count, random_number,fitness=-1):
+        self.clusters = clusters
+        self.sampling_interval = sampling_interval 
+        self.variation_count = variation_count 
+        self.random_number = random_number
+        self.fitness = fitness
+        self.centers = centers
+        
+    def random_number(self):
+        self.random_number = random.random()
+        
+    #def describe(self):
+        #print("Candidate with features={}, fitness={}".format(self.clusters.describe(),self.fitness))
+    
+    
